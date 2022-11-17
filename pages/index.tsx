374777520a6a9b6f634f7aafa715bc9bd40b1b2e
cleats01 <cleats01@gmail.com>
2022-11-17 10:48:17 +0900

    chore: 초기 세팅
    Next.js, Typescript, Styled-Components

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyle from '../styles/global-styles';
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div>hi</div>
+    </React.Fragment>
+  );
+}
