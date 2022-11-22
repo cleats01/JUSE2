8a47e3bad197bd6921c6440707d59d55121030ab
cleats01 <cleats01@gmail.com>
2022-11-22 13:16:31 +0900

    design: theme 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import NavBar from '../components/NavBar';
 import GlobalStyle from '../styles/global-styles';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <GlobalStyle />
-      <NavBar />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <NavBar />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
