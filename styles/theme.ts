00a873c0d66444ded5b38c4126ceb56e33501006
cleats01 <cleats01@gmail.com>
2022-11-22 13:18:04 +0900

    design: 테마 타입 설정

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,25 +1,5 @@
 import { DefaultTheme } from 'styled-components';
 
-const theme = {
-  breakPoints: {
-    mobile: '480px',
-    tablet: '768px',
-    desktop: '1024px',
-    // 콘텐츠 최대 width: 1180px
-  },
-  colors: {
-    black1: '#333533',
-    grey1: '#e9ecef',
-    grey2: '#dee2e6',
-    grey3: '#adb5bd',
-    grey4: '#7e858d',
-    grey5: '#495057',
-    purple1: `#be99ff`,
-    purple2: '#6f2dbd',
-    tiffanyblue: '#81d8cf',
-  },
-};
-
 const colors = {
   black1: '#333533',
   grey1: '#e9ecef',
@@ -38,52 +18,12 @@ const fontSize = {
   content: 14,
 };
 
+export type ColorsTypes = typeof colors;
+export type FontSizeTypes = typeof fontSize;
+
+const theme: DefaultTheme = {
+  colors,
+  fontSize,
+};
+
 export default theme;
-
-export const darkTheme = {
-  body: '#292929',
-  text: '#FAFAFA',
-  background: '#292929',
-  breakPoints: {
-    mobile: '480px',
-    tablet: '768px',
-    desktop: '1024px',
-    // 콘텐츠 최대 width: 1180px
-  },
-  colors: {
-    black1: '#FAFAFA',
-    grey1: '#7e858d',
-    grey2: '#495057',
-    grey3: '#495057',
-    grey4: '#b4b4b4',
-    grey5: '#e9ecef',
-    purple0: '#f2caff',
-    purple1: `#be99ff`,
-    purple2: '#8c6acb',
-    tiffanyblue: '#81d8cf',
-  },
-};
-
-export const lightTheme = {
-  body: '#FFFFFF',
-  text: '#333533',
-  background: '#FFFFFF',
-  breakPoints: {
-    mobile: '480px',
-    tablet: '768px',
-    desktop: '1024px',
-    // 콘텐츠 최대 width: 1180px
-  },
-  colors: {
-    black1: '#333533',
-    grey1: '#e9ecef',
-    grey2: '#dee2e6',
-    grey3: '#adb5bd',
-    grey4: '#7e858d',
-    grey5: '#495057',
-    purple0: '#f2caff',
-    purple1: `#be99ff`,
-    purple2: '#8c6acb',
-    tiffanyblue: '#81d8cf',
-  },
-};
