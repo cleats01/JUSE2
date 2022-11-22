a45e4c50c2034dd6dbf581c031447df0649dd650
cleats01 <cleats01@gmail.com>
2022-11-22 16:50:19 +0900

    design: 아이콘 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,10 +1,10 @@
 import { DefaultTheme } from 'styled-components';
 
 const colors = {
-  black1: '#333533',
-  grey1: '#e9ecef',
-  grey2: '#dee2e6',
-  grey3: '#adb5bd',
+  black1: '#333533', // 기본 텍스트
+  grey1: '#e9ecef', // 네비 경계
+  grey2: '#dee2e6', // 구분선
+  grey3: '#adb5bd', // 비활성화
   grey4: '#7e858d',
   grey5: '#495057',
   purple1: `#be99ff`,
