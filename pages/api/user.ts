444782fa08d2d15dcf310c8e95ed7eca0433e13a
cleats01 <cleats01@gmail.com>
2022-11-30 21:24:22 +0900

    chore: 주석 삭제

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -16,8 +16,6 @@ export default async function handle(
     switch (req.method) {
       case 'GET': {
         if (req.query.id) {
-          // Get a single user if id is provided is the query
-          // api/users?id=1
           const user = await getUserById(req.query.id as string);
           return res.status(200).json(user);
         } else if (req.query.email) {
@@ -29,19 +27,16 @@ export default async function handle(
         }
       }
       case 'POST': {
-        // Create a new user
         const { email, nickname } = req.body;
         const user = await createUser(email, nickname);
         return res.json(user);
       }
       case 'PUT': {
-        // Update an existing user
         const { id, ...updateData } = req.body;
         const user = await updateUser(id, updateData);
         return res.json(user);
       }
       case 'DELETE': {
-        // Delete an existing user
         const { id } = req.body;
         const user = await deleteUser(id);
         return res.json(user);
