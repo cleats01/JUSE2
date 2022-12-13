bfa70cba0a7d2d5d05ff8f83da8d3e2e93f8b039
cleats01 <cleats01@gmail.com>
2022-12-13 11:49:49 +0900

    fix: 북마크시 board 데이터에서도 북마크 수 증가

--- pages/api/bookmarks.ts
+++ pages/api/bookmarks.ts
@@ -19,6 +19,7 @@ export default async function handler(
     switch (req.method) {
       case 'POST': {
         if (user?.bookmarkList.includes(req.query.boardId as string)) {
+          // 이미 북마크가 되어있는 경우
           const removedList = user.bookmarkList.filter(
             (id) => id !== req.query.boardId
           );
@@ -26,10 +27,19 @@ export default async function handler(
             where: { id: user?.id as string },
             data: { bookmarkList: removedList },
           });
+          await prisma.board.update({
+            where: { id: req.query.boardId as string },
+            data: { bookmark: { decrement: 1 } },
+          });
         } else {
+          // 북마크가 되어있지 않은 경우
           await prisma.user.update({
             where: { id: user?.id as string },
             data: { bookmarkList: { push: req.query.boardId } },
+          });
+          await prisma.board.update({
+            where: { id: req.query.boardId as string },
+            data: { bookmark: { increment: 1 } },
           });
         }
         return res.status(200).json({ message: 'bookmarked' });
