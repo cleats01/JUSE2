e00bb7276bea61b7d4eb1d28fe33613f6d17c58d
cleats01 <cleats01@gmail.com>
2023-01-05 16:45:55 +0900

    refactor: 게시물 채팅 리스트 페이지 컴포넌트 정리

---
+++ components/Board/BoardChatPage.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NavbarTextOnly from 'components/NavbarTextOnly';
+import ChatList from 'components/Chat/ChatList';
+
+export default function BoardChatPage() {
+  return (
+    <ChatListLayout>
+      <NavbarTextOnly centerText='채팅목록' back />
+      <ChatList />
+    </ChatListLayout>
+  );
+}
+
+const ChatListLayout = styled.div`
+  padding: 65px 0;
+`;
--- pages/board/[boardId]/chat.tsx
+++ pages/board/[boardId]/chat.tsx
@@ -1,42 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import BoardChatPage from 'components/Board/BoardChatPage';
 
-import { useSession } from 'next-auth/react';
-import styled from 'styled-components';
-import TabBar from '../../../components/TabBar';
-import NavbarTextOnly from '../../../components/NavbarTextOnly';
-import { getChatListByBoardId } from '../../../utils/axios';
-import { Room } from '@prisma/client';
-import ChatListItem from '../../../components/Chat/ChatListItem';
-import { useRouter } from 'next/router';
-
-export default function ChatListPage() {
-  const { data: session, status } = useSession();
-  const [chatList, setChatList] = useState<Room[]>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === 'authenticated') {
-      getChatListByBoardId(router.query.boardId as string).then((res) => {
-        setChatList(res);
-      });
-    }
-  }, [status]);
-
-  return (
-    <ChatListLayout>
-      <NavbarTextOnly centerText='채팅목록' back />
-      <ChatListContainer>
-        {chatList?.map((chattingRoom) => (
-          <ChatListItem key={chattingRoom.id} data={chattingRoom as IRoom} />
-        ))}
-      </ChatListContainer>
-      <TabBar />
-    </ChatListLayout>
-  );
+export default function BoardChat() {
+  return <BoardChatPage />;
 }
-
-const ChatListLayout = styled.div``;
-
-const ChatListContainer = styled.div`
-  padding: 65px 0;
-`;
