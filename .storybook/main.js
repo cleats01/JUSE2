293ccf4aacb047e7d85078eb42876d0de5702b00
cleats01 <cleats01@gmail.com>
2023-01-30 15:40:02 +0900

    feat: storybook 절대경로 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
@@ -13,5 +14,9 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
   },
 };
