3bfe214d1b5f8d761f1b5ca4058e3a77d312f4a3
cleats01 <cleats01@gmail.com>
2023-01-30 15:44:03 +0900

    delete: 불필요한 옵션 제거

--- stories/atoms/LogoText.stories.tsx
+++ stories/atoms/LogoText.stories.tsx
@@ -4,7 +4,6 @@ import LogoText, { ILogoProps } from './LogoText';
 export default {
   title: 'components/atoms/LogoText',
   component: LogoText,
-  argTypes: { size: { type: 'number', defaultValue: 24 } },
 };
 
 const Template = (args: ILogoProps) => <LogoText {...args} />;
