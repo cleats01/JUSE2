7dad6dc76618209f0d7de0fb63f5a0384021b143
cleats01 <cleats01@gmail.com>
2023-01-31 13:44:26 +0900

    fix: Next/Image 컴포넌트 optimization 비활성화

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,11 @@ import { ThemeProvider } from 'styled-components';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import GlobalStyle from '../stories/foundations/global.ts';
 import theme, { palette } from '../styles/theme';
+import * as NextImage from 'next/image';
+
+NextImage.defaultProps = {
+  unoptimized: true,
+};
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
