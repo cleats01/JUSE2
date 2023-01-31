36e8d28c2e5b06857dc045d67131abc8e9ffc748
cleats01 <cleats01@gmail.com>
2023-01-31 13:44:46 +0900

    feat: 고정위치버튼 컴포넌트 storybook 작성

---
+++ stories/atoms/ButtonFixedPosition.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { ReactNode } from 'react';
+
+export interface IButtonFixedPositionProps {
+  children: ReactNode;
+  bottom: number;
+  visible?: boolean;
+  size?: number;
+  borderColor?: string;
+}
+
+export default function ButtonFixedPosition(props: IButtonFixedPositionProps) {
+  return <PositionContainer {...props}>{props.children}</PositionContainer>;
+}
+
+const PositionContainer = styled.div<{
+  visible?: boolean;
+  size?: number;
+  bottom: number;
+  borderColor?: string;
+}>`
+  position: fixed;
+  display: ${({ visible }) => (visible ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  width: ${({ size }) => (size ? `${size}px` : '45px')};
+  height: ${({ size }) => (size ? `${size}px` : '45px')};
+  bottom: ${({ bottom }) => `${bottom}px`};
+  right: max(calc(50% - 205px), 35px);
+  z-index: 10;
+  color: ${({ borderColor }) => borderColor};
+  border-radius: 50%;
+  border: 2px solid ${({ borderColor }) => borderColor};
+  background-color: #fff;
+  cursor: pointer;
+`;
