053fba8df025700b4e43ab9ae36f08cb8d2ce9e7
cleats01 <cleats01@gmail.com>
2023-02-14 10:47:16 +0900

    fix: api 요청 절대주소로 변경

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 // BOARDS
 export const getBoards = (query: string) =>
-  axios.get('/api/boards' + query).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/boards` + query)
+    .then((res) => res.data);
 
 export const getBoardsBySearch = (search: string) =>
-  axios.get(`/api/boards?search=${search}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/boards?search=${search}`)
+    .then((res) => res.data);
 
 export const getBoardById = (boardId: string) =>
   axios
@@ -18,16 +22,18 @@ export const getRelated = (boardId: string) =>
     .then((res) => res.data);
 
 export const postBoard = (data: boardFormData) =>
-  axios.post('/api/boards', data);
+  axios.post(`${process.env.BASE_URL}/api/boards`, data);
 
 export const patchBoardClose = (boardId: string, isClosed: boolean) =>
-  axios.patch(`/api/boards/${boardId}?isClosed=${!isClosed}`);
+  axios.patch(
+    `${process.env.BASE_URL}/api/boards/${boardId}?isClosed=${!isClosed}`
+  );
 
 export const patchBoard = (boardId: string, data: boardFormData) =>
-  axios.patch(`/api/boards/${boardId}`, data);
+  axios.patch(`${process.env.BASE_URL}/api/boards/${boardId}`, data);
 
 export const deleteBoard = (boardId: string) =>
-  axios.delete(`/api/boards/${boardId}`);
+  axios.delete(`${process.env.BASE_URL}/api/boards/${boardId}`);
 
 // USERS
 export const getUserById = (userId: string) =>
@@ -36,10 +42,14 @@ export const getUserById = (userId: string) =>
     .then((res) => res.data);
 
 export const getUsersByIds = (userIds: string[]) =>
-  axios.get(`/api/users?ids=${userIds}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/users?ids=${userIds}`)
+    .then((res) => res.data);
 
 export const getNickname = (nickname: string) =>
-  axios.get(`/api/users?nickname=${nickname}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/users?nickname=${nickname}`)
+    .then((res) => res.data);
 
 export const getNewbies = () =>
   axios
@@ -51,21 +61,23 @@ export const postUser = (data: {
   nickname: string;
   userTechStack: string[];
   image: string;
-}) => axios.post('/api/users', data);
+}) => axios.post(`${process.env.BASE_URL}/api/users`, data);
 
 export const patchUser = (data: {
   id: string;
   nickname: string;
   userTechStack: string[];
   image: string;
-}) => axios.patch('/api/users', data);
+}) => axios.patch(`${process.env.BASE_URL}/api/users`, data);
 
 export const deleteUser = (userId: string) =>
-  axios.delete(`/api/users?id=${userId}`);
+  axios.delete(`${process.env.BASE_URL}/api/users?id=${userId}`);
 
 // MY
 export const getMyLists = (userId: string) =>
-  axios.get(`/api/boards/my?id=${userId}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/boards/my?id=${userId}`)
+    .then((res) => res.data);
 
 // LIKES
 export const getIsLiked = (userId: string) =>
@@ -78,7 +90,7 @@ export const postLikes = (userId: string) =>
 
 // BOOKMARKS
 export const postBookmarks = (boardId: string) =>
-  axios.post(`/api/bookmarks?boardId=${boardId}`);
+  axios.post(`${process.env.BASE_URL}/api/bookmarks?boardId=${boardId}`);
 
 // APPLICATIONS
 export const postApplications = (
@@ -87,7 +99,7 @@ export const postApplications = (
   applicantId: string
 ) =>
   axios.post(
-    `/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}`
+    `${process.env.BASE_URL}/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}`
   );
 
 export const deleteApplications = (
@@ -96,7 +108,7 @@ export const deleteApplications = (
   applicantId: string
 ) =>
   axios.delete(
-    `/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}`
+    `${process.env.BASE_URL}/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}`
   );
 
 export const patchApplications = (
@@ -106,28 +118,36 @@ export const patchApplications = (
   where: string
 ) =>
   axios.patch(
-    `/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}&to=${where}`
+    `${process.env.BASE_URL}/api/applications?boardId=${boardId}&position=${positionName}&applicantId=${applicantId}&to=${where}`
   );
 
 // CHAT
 export const getChattingRoom = (chatId: string) =>
-  axios.get(`/api/chat/${chatId}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/chat/${chatId}`)
+    .then((res) => res.data);
 
 export const postChattingRoom = (membersId: string[], boardId?: string) => {
   if (boardId) {
-    return axios.post(`/api/chat?boardId=${boardId}`, { membersId });
+    return axios.post(`${process.env.BASE_URL}/api/chat?boardId=${boardId}`, {
+      membersId,
+    });
   }
-  return axios.post(`/api/chat`, { membersId });
+  return axios.post(`${process.env.BASE_URL}/api/chat`, { membersId });
 };
 
 export const getChatList = (userId: string) =>
-  axios.get(`/api/chat?userId=${userId}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/chat?userId=${userId}`)
+    .then((res) => res.data);
 
 export const getChatListByBoardId = (boardId: string) =>
-  axios.get(`/api/chat?boardId=${boardId}`).then((res) => res.data);
+  axios
+    .get(`${process.env.BASE_URL}/api/chat?boardId=${boardId}`)
+    .then((res) => res.data);
 
 export const postMessage = (chatId: string, data: IMessage) =>
-  axios.post(`/api/chat/${chatId}`, {
+  axios.post(`${process.env.BASE_URL}/api/chat/${chatId}`, {
     data,
   });
 
@@ -139,4 +159,4 @@ export const getTrendings = () =>
 
 // REPORT
 export const postReport = (data: { content: string }) =>
-  axios.post('/api/reports', data);
+  axios.post(`${process.env.BASE_URL}/api/reports`, data);
